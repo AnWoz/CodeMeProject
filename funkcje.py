986e7b1371577a60f77de618d78ebc608901ed97
AnWoz <anna.woziwodzka@gmail.com>
2018-12-01 20:06:25 +0100

    Revised version

---
+++ funkcje.py
@@ -0,0 +1,117 @@
+import json
+from datetime import datetime
+
+def wczytaj_z_pliku(nazwaPliku):
+    try:
+        f = open(nazwaPliku)
+    except FileNotFoundError:
+        dane = []
+    else:
+        dane = json.load(f)
+        f.close()
+    return dane
+
+
+def zapisz_do_pliku(nazwa, dane):
+    with open(nazwa, 'w') as f:
+        json.dump(dane, f)
+
+def wczytaj_czas():
+    teraz = str(datetime.now())
+    return teraz
+
+def wczytaj_dane(zapytanie):
+    wczytane = input(zapytanie)
+    return wczytane
+
+
+def nowy_pacjent(naglowki, dane, IDlist):
+    if not IDlist:
+        count = 0
+    else:
+        count = IDlist[-1]
+    pacjent = {naglowki[0]: count + 1}
+
+    pacjent[naglowki[1]] = wczytaj_czas()
+    for klucz in naglowki[2:]:
+        while True:
+            wpis = input('Insert '+klucz+':')
+            if klucz == naglowki[2] and wpis not in ['M', 'F']:
+                print('Insert a valid information: F for female, M for male')
+            elif klucz in naglowki[3:6]and not wpis.isdigit():
+                print('Insert a valid number')
+            else:
+                pacjent[klucz] = wpis
+                break
+
+    dane.append(pacjent)
+    print('Database updated!')
+
+    return dane
+
+
+def modyfikacja_wpisu(naglowki, dane, IDlist):
+    while True:
+        print(IDlist)
+        ktory_pacjent = wczytaj_dane('Which patient ID you want to modify?')
+        ktory_pacjent_int = int(ktory_pacjent)
+        if ktory_pacjent_int in IDlist:
+            break
+        else:
+            print('You must select a valid ID!')
+    while True:
+        print(naglowki[2:])
+        jakie_dane = wczytaj_dane('Which information from the above list you want to modify?')
+        if jakie_dane in naglowki[2:]:
+            break
+        else:
+            print('You must select a valid information name!')
+    while True:
+        nowe_dane = wczytaj_dane('Insert a new value:')
+        if nowe_dane:
+            break
+        else:
+            print('You must insert a valid value!')
+    for n in dane:
+        if n['ID'] == ktory_pacjent:
+            n[jakie_dane] = nowe_dane
+            print('Entry updated!')
+    return dane
+
+
+def nowa_kolumna(naglowki, dane):
+    nowa_kol = wczytaj_dane('Insert new column title:')
+    if nowa_kol in naglowki:
+        print('This column exists!')
+    else:
+        naglowki.append(nowa_kol)
+        print(naglowki)
+        for n in dane:
+            n[nowa_kol] = ''
+    return dane, naglowki
+
+
+def eksport_wybranych(dane, IDlist):
+    lista_wybrane = []
+    lista_wybrane_rekordy = []
+    while True:
+        wybor_pacjenta = wczytaj_dane('Select patient ID. If all selected, press 0')
+        if wybor_pacjenta == '0':
+            break
+        if int(wybor_pacjenta) not in IDlist:
+            print('This ID is not in a database.')
+            continue
+        lista_wybrane.append(wybor_pacjenta)
+
+    if lista_wybrane:
+        nazwa_pliku = '_'.join(lista_wybrane) + '.json'
+        for ID in lista_wybrane:
+            for n in dane:
+                if n['ID'] == int(ID):
+                    lista_wybrane_rekordy.append(n)
+
+        print('Selected IDs for export:', lista_wybrane)
+        zapisz_do_pliku(nazwa_pliku, lista_wybrane_rekordy)
+    else:
+        print('No IDs selected!')
+
---
+++ projekt_funkcje.py
@@ -0,0 +1,53 @@
+from funkcje import *
+
+baza = wczytaj_z_pliku('HBV_database.json')
+
+if not baza:
+    klucze = ['ID', 'entry_date', 'sex_(M/F)', 'age', 'BMI', 'years_after_diagnosis']
+else:
+    klucze = []
+    for key in baza[0].keys():
+        klucze.append(key)
+
+print('Welcome to HBV 2018 database')
+
+while True:
+    lista_ID = [n['ID'] for n in baza]
+    print('****************')
+    print('''What can I do for you?
+    New patient entry: press 1
+    Entry modification: press 2
+    Add new column: press 3
+    Print existing database: press 4
+    Print data for selected patient: press 5
+    Close database: press 0
+    ''')
+    selection = input()
+
+    if baza == [] and selection == '2':
+        print('The database is empty!')
+        break
+
+    if selection == '1':
+        nowy_pacjent(naglowki=klucze, dane=baza, IDlist=lista_ID)
+
+    if selection == '2':
+        modyfikacja_wpisu(naglowki=klucze, dane=baza, IDlist=lista_ID)
+
+    if selection == '3':
+        nowa_kolumna(naglowki=klucze, dane=baza)
+
+    if selection == '4':
+        print(baza)
+
+    if selection == '5':
+        eksport_wybranych(dane=baza, IDlist=lista_ID)
+
+    if selection == '0':
+        break
+
+print('Thank you for using HBV 2018 database!\nThe database is as follows:')
+for rekord in baza:
+    print(rekord, '\n')
+
+zapisz_do_pliku('HBV_database.json', baza)
